00062e63f3b5029ab4c05d8199ac05b3f1e9ed6c
Laksh Advani <laksh.advani@gmail.com>
2018-07-24 13:53:28 -0700

    Update bigrams.py

--- bigrams.py
+++ bigrams.py
@@ -4,7 +4,7 @@ from operator import itemgetter
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
-print("markov")
+print("markov chain experiment")
 royal_probability = 0
 
 
